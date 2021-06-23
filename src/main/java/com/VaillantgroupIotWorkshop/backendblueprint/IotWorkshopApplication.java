0d3eec23bf3bebb88011a5f64ee4c681c07b2292
Jonas Eller <jonas.eller@vaillant-group.com>
2021-06-23 10:42:48 +0200

    create repo

---
+++ src/main/java/com/VaillantgroupIotWorkshop/backendblueprint/IotWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.VaillantgroupIotWorkshop.backendblueprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/VaillantgroupIotWorkshop/backendblueprint/IotWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VaillantgroupIotWorkshop.backendblueprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
