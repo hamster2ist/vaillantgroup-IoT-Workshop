455ab611d4bebdf9e839c0ac3f4099f397307795
Jonas Eller <jonas.eller@vaillant-group.com>
2021-07-27 14:16:06 +0200

    change application yaml -> mongodb uri from local host to mongodb.default.svc.cluster.local

--- src/main/java/com/vaillantgroup/iot/workshop/backendblueprint/IotWorkshopApplication.java
+++ src/main/java/com/vaillantgroup/iot/workshop/backendblueprint/IotWorkshopApplication.java
@@ -10,19 +10,5 @@ public class IotWorkshopApplication {
 		SpringApplication.run(IotWorkshopApplication.class, args);
 	}
 
-	/*@Bean
-	CommandLineRunner runner(HumidityRepository repository){
-
-		return args -> {
-			DeviceMetadataDTO deviceMetadataDTO = new DeviceMetadataDTO("Test");
-			HumidityDTO humidityDTO = new HumidityDTO(
-					10,
-					deviceMetadataDTO
-			);
-
-			repository.insert(humidityDTO);
-		};
-	}*/
-
 }
 
